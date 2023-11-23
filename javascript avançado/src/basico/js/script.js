f6ee84fdbed46d40cf0ef98a41330d3d2d720625
gabrieldonisete <144962030+gabrieldonisete@users.noreply.github.com>
2023-11-23 02:08:39 -0300

    Add files via upload

--- javascript avançado/src/basico/js/script.js
+++ javascript avançado/src/basico/js/script.js
@@ -1,163 +1,111 @@
-function calcularMedia( notas ) {
-
+const calcularMedia = (notas) => {
     let soma = 0;
-    for( c = 0; c < notas.length; c++) {
+    for (let c = 0; c < notas.length; c++) {
         soma += notas[c];
     }
-
     media = soma / notas.length;
-
     return media;
-
 }
 
 let media; // escopo global
 
-function aprovacao( notas ) {
-
-    let media = calcularMedia( notas ); // escopo da função
-
+const aprovacao = (notas) => {
+    let media = calcularMedia(notas); // escopo da função
     let condicao = media >= 8 ? "aprovado" : "reprovado";
-
     return 'Média: ' + media + ' - Resultado: ' + condicao;
-
 }
 
-
-// Função Recursivas
-
-function contagemRegressiva(numero){
-
-    console.log(numero);  
-    
+const contagemRegressiva = (numero) => {
+    console.log(numero);
     let proximoNumero = numero - 1;
-
-    if(proximoNumero > 0)
+    if (proximoNumero > 0)
         contagemRegressiva(proximoNumero);
-
 }
 
-// contagemRegressiva(50);
-
-/* 
- * Formulário envio de dados para cálculo da média 
- */
 const formulario1 = document.getElementById('formulario-01');
 
-if(formulario1)
-    formulario1.addEventListener('submit', function( evento ){
-
+if (formulario1)
+    formulario1.addEventListener('submit', (evento) => {
         evento.preventDefault();
         evento.stopPropagation();
-
-        if( this.getAttribute('class').match(/erro/) ) {
+        if (formulario1.getAttribute('class').match(/erro/)) {
             return false;
         }
-        
-        let dados = new FormData(this);
-
+        let dados = new FormData(formulario1);
         let notas = [];
-
-        for(let key of dados.keys()) {
-
+        for (let key of dados.keys()) {
             let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
-
-            if(!isNaN(numero)) {
+            if (!isNaN(numero)) {
                 notas.push(numero);
             }
-
         }
-
         console.log(notas);
-
         texto = aprovacao(notas)
-
         document.getElementById('resultado').innerHTML = texto;
-
     });
 
-
-function validaCampo(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
+const validaCampo = (elemento) => {
+    elemento.addEventListener('focusout', (event) => {
         event.preventDefault();
-
-        if(this.value == ""){
+        if (elemento.value == "") {
             document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
+            elemento.classList.add('erro');
+            elemento.parentNode.classList.add('erro');
             return false;
         } else {
             document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
+            elemento.classList.remove('erro');
+            elemento.parentNode.classList.remove('erro');
         }
-
     });
-
 }
 
-function validaCampoNumerico(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
+const validaCampoNumerico = (elemento) => {
+    elemento.addEventListener('focusout', (event) => {
         event.preventDefault();
-
-        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
-
-        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
+        let numero = elemento.value.match(/^[\d]5-[\d]3/) ? elemento.value.replace(/-/, "") : elemento.value;
+        if (numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10) {
             document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
+            elemento.classList.remove('erro');
+            elemento.parentNode.classList.remove('erro');
         } else {
             document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
+            elemento.classList.add('erro');
+            elemento.parentNode.classList.add('erro');
             return false;
         }
-
     });
-
 }
 
-
-function validaEmail(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
+const validaEmail = (elemento) => {
+    elemento.addEventListener('focusout', (event) => {
         event.preventDefault();
-
         const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
-        if(this.value.match(emailValido)) {
+        if (elemento.value.match(emailValido)) {
             document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
+            elemento.classList.remove('erro');
+            elemento.parentNode.classList.remove('erro');
         } else {
             document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
+            elemento.classList.add('erro');
+            elemento.parentNode.classList.add('erro');
             return false;
         }
-
     });
-
 }
-
 
 let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
 let camposNumericos = document.querySelectorAll('input.numero');
 let camposEmail = document.querySelectorAll('input.email');
 
-for( let emFoco of camposObrigatorios) {
+for (let emFoco of camposObrigatorios) {
     validaCampo(emFoco);
 }
 
-for( let emFoco of camposNumericos) {
+for (let emFoco of camposNumericos) {
     validaCampoNumerico(emFoco);
 }
 
-for( let emFoco of camposEmail) {
+for (let emFoco of camposEmail) {
     validaEmail(emFoco);
 }
-
